fbb4ea8bd2082bc026f60ef71156f1f788cc374a
DBurke <burkedion52@gmail.com>
2022-05-04 20:49:53 -0700

    fixed code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,27 @@
 import * as React from "react";
 import DeviceChart from "./DeviceChart";
 import Layout from "./Layout";
+import { name } from "../utils/generators";
+import RadialChart from "./Gradiant"
 
 export default function Home() {
   return (
     <Layout>
-      <DeviceChart />
+      <h2>{name("Jordan", "Hudgens")}</h2>
+      <DeviceChart
+        options={{
+          colors: ["#e302a7", "#07e3b0", "#f58e20"],
+          labels: ["Apple", "Android", "Pixel"],
+        }}
+      />
+
+      <DeviceChart
+        options={{
+          colors: ["#910606", "#320691", "#910647"],
+          labels: ["Smartphones", "laptops", "ipads"],
+        }}
+      />
+      <RadialChart />
     </Layout>
   );
 }
